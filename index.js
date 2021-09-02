996369a6de593b116eb2fe68933ab040d7e2a134
pratul20 <82647665+pratul20@users.noreply.github.com>
2021-09-02 22:57:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -152,12 +152,6 @@ function gameEngine() {
         board.append(specialFoodElement);
         
     }
-    if(specialCollected==2) {
-        setTimeout(() => {
-            specialCollected=0;
-        }, 5000);
-    }
-
 }
 
 
